7339302bf06cc855d4e37e620dee6076b787eef9
jegomez <jegomez@veracruz.gob.mx>
2023-04-14 18:02:25 -0600

    Screens for text and data response

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,50 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
+
+  const response = {
+    feelings: ["contento", "alegre"],
+    steps: ["disfrutar del día", "tener una actitud positiva"]
+  }
+
   return (
-    <main className="flex min-h-screen h-screen">
-      <div className='before:flex before:justify-center before:blur-2xl before:fixed before:-z-10 before:w-[400px] before:h-[400px] before:bg-orange-500 before:rounded-full'>
-        <input type="text" />
+    <main className="flex justify-center items-center min-h-screen h-screen">
+     
+      <section className="flex flex-row gap-2 flex-wrap">
+        <article className="backdrop-blur-3xl bg-white/60 rounded-xl min-w-[400px] h-auto shadow-sm">
+          <textarea className="p-3 outline-none resize-none bg-transparent overflow-auto h-full w-full" placeholder="Escribe lo que sientes..."></textarea>
+        </article>
+        <article className="flex flex-col gap-2 flex-wrap">
+          <div id="emojis" className="flex items-center gap-2 p-4 min-w-[400px] backdrop-blur-3xl rounded-xl bg-white/60 rounded-x h-[100px]">
+            { 
+              response.feelings.map((feel, index)=> {
+                return (
+                  <span className="p-2 bg-blue-200 rounded-xl border-blue-400 border-[1px] text-white" key={index}>{feel}</span>
+                )
+              }) 
+            }
+          </div>
+          <div id="emojis" className="flex flex-col gap-2 p-4 min-w-[400px] backdrop-blur-3xl rounded-xl bg-white/60 rounded-x h-[300px]">
+            {
+              response.steps.map((step, index) => {
+                return (
+                  <span className="bg-white pl-2 pr-2 pt-3 pb-3 rounded-xl shadow-sm" key={index}>{step}</span>
+                )
+              })
+            } 
+          </div>
+        </article>
+      </section>
+
+      <div 
+        id="square" 
+        className="absolute blur-[72px] w-[300px] h-[300px] bg-gradient-to-r from-orange-600 to-orange-300 -z-10 top-[100px] left-[250px] animate-[bounce_6s_ease-in-out_infinite]">
+      </div>
+      <div
+        id="square"
+        className="absolute blur-[72px] w-[200px] h-[200px] bg-gradient-to-r from-blue-500 to-cyan-500 -z-10 top-[300px] left-[600px] animate-[bounce_4s_ease-in-out_infinite]">
+      </div>
+      <div
+        id="square"
+        className="absolute blur-[72px] w-[200px] h-[200px] bg-gradient-to-r from-green-600 to-lime-700 -z-10 top-[80px] right-[350px] animate-[bounce_10s_ease-in-out_infinite]">
       </div>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        bounce_slow: 'boounce_slow 3s ease-in-out infinite',
+      },
+      keyframes: {
+        bounce_slow: {
+          '0%, 100%': {
+            transform: 'translateY(-55%)'
+          },
+          '50%' : {
+            transform: 'translateY(0)'
+          }
+        }
+      }
     },
   },
   plugins: [],
