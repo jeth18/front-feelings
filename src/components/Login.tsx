0a2a0741ba149bff98ba2b472979a56b8f5b907c
jegomez <jegomez@veracruz.gob.mx>
2023-04-28 19:43:18 -0600

    LOGIN

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,7 +8,12 @@ export const Login = () => {
   const show = !session ? 'display-modal' : 'display-none'
   return (
     <div className={`modal ${show} z-50`}>
-      <section className='modal-main'>
+      <article className='modal-main'>
+        <section className="flex flex-col justify-center items-center gap-4 pt-4">
+          <p>Bienvenido a</p>
+          <p id="title-project" className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500">HOW FEELINGS</p>
+          <p className="text-center max-w-[400px] text-sm">Un sitio donde puedes escribir lo que sientes, saber como te sientes y recibir consejos.</p>
+        </section>
         <section className='flex flex-col gap-2 p-4'>
           <button className="bg-gray-800 rounded-md p-2 pt-3 pb-3 shadow-md text-white flex gap-2 min-w-[300px] justify-center items-center" onClick={() => signIn("github")}>
             <Image src={GitHub} alt="github" width={32} height={32} />
@@ -19,7 +24,7 @@ export const Login = () => {
             Continuar con Google
           </button>
         </section>
-      </section>
+      </article>
     </div>
   )
   
